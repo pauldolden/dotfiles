62178931fd0c9643f4384f563ee0e81122878adc
Paul Dolden <paul@dolden.co.uk>
2022-12-09 10:51:27 +0000

    changes

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,6 +1,6 @@
 vim.opt.background = "dark"
 
 vim.cmd("colorscheme tokyonight-night")
--- vim.cmd("colorscheme catppuccin")
+-- vim.cmd("colorscheme catppuccin-mocha")
 -- vim.cmd("colorscheme shades_of_purple")
 
--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -34,93 +34,24 @@ local function config(_config)
 	}, _config or {})
 end
 
--- TypeScript
-require('lspconfig').tsserver.setup(config())
--- HTML
-require('lspconfig').html.setup(config())
--- ESLint
-require('lspconfig').eslint.setup(config({
-    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue", "astro", "svelte" }
-}))
--- -- Go
-require("lspconfig").gopls.setup(config({
-	cmd = { "gopls", "serve" },
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-			},
-			staticcheck = true,
-		},
-	},
-}))
-vim.g.go_def_mode='gopls'
-vim.g.go_info_mode='gopls'
--- Rust
---require("lspconfig").rust_analyzer.setup(config({
---	cmd = { "rustup", "run", "nightly", "rust-analyzer" },
---	--[[
---    settings = {
---        rust = {
---            unstable_features = true,
---            build_on_save = false,
---            all_features = true,
---        },
---    }
---    --]]
---}))
--- Svelte
-require'lspconfig'.svelte.setup(config())
 vim.g.vim_svelte_plugin_load_full_syntax = 1
 vim.g.vim_svelte_plugin_use_typescript = 1
 vim.g.vim_svelte_plugin_use_sass = 1
--- Tailwind
-require'lspconfig'.tailwindcss.setup(config())
--- Lua
-require'lspconfig'.sumneko_lua.setup(config())
--- Setup Completion
-local cmp = require("cmp")
-local lspkind = require('lspkind')
 
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-           require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-         end,
-       },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      formatting = {
-        format = lspkind.cmp_format({
-          mode = 'symbol', -- show only symbol annotations
-          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
-          -- The function below will be called before any actual modifications from lspkind
-          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-          before = function (entry, vim_item)
-            return vim_item
-          end
-        })
-      },
-      mapping = {
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<Esc>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<TAB>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<S-k>'] = cmp.mapping.select_prev_item(),
-        ['<S-j>'] = cmp.mapping.select_next_item(),
-      },
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' }, -- For luasnip users.
-        { name = 'buffer' },
-        { name = "path" },
-      }
-    })
- 
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup(config())
+        end,
+}
+-- These don't work?
+require('lspconfig').eslint.setup(config({
+    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue", "astro", "svelte" }
+}))
+require('lspconfig').markdownlint.setup {}
