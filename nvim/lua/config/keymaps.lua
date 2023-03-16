e54f874710dc52c19a6e54934867a0c77b8d6e9d
Paul Dolden <paul@dolden.co.uk>
2023-03-16 08:37:09 +0000

    fix

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,6 @@ vim.api.nvim_set_keymap("n", "<leader>t", ":Trouble<CR>", { noremap = true, sile
 vim.api.nvim_set_keymap("n", "<leader>yp", '<cmd>let @+ = expand("%:p")<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-x>[", ":BufferLineCloseLeft<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-x>]", ":BufferLineCloseRight<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-x>.", ":BufferLinePickClose<CR>", { noremap = true, silent = true })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,16 +3,5 @@
 -- Add any additional options here
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.title = true
--- set window title to root directory
+vim.opt.title = false
 vim.opt.compatible = false
--- Vimwiki stuff
-vim.cmd("filetype plugin on")
-vim.cmd("syntax on")
-vim.g.vimwiki_list = {
-  {
-    path = "~/vault/",
-    syntax = "markdown",
-    ext = ".md",
-  },
-}
