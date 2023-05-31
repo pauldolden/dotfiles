7d9bad27de3ef47e24845ff6bb863cbda48a529b
Paul Dolden <p.dolden@comicrelief.com>
2023-05-31 14:21:22 +0100

    colour theme change and tab bar fix for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,60 +17,69 @@ config.font_size = 17.0
 config.tab_bar_at_bottom = true
 config.show_tab_index_in_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
+config.use_fancy_tab_bar = false
+config.tab_max_width = 50
 
 config.window_frame = {
 	font_size = 16.0,
 }
 
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab_title(tab)
+	if tab then
+		return {
+			{ Text = "  " .. title .. "  " },
+		}
+	end
+	return title
+end)
+
 config.colors = {
-	foreground = "#c0caf5",
-	background = "#1a1b26",
-	cursor_bg = "#c0caf5",
-	cursor_border = "#c0caf5",
-	cursor_fg = "#1a1b26",
-	selection_bg = "#283457",
-	selection_fg = "#c0caf5",
-	ansi = {
-		"#15161e",
-		"#f7768e",
-		"#9ece6a",
-		"#e0af68",
-		"#7aa2f7",
-		"#bb9af7",
-		"#7dcfff",
-		"#a9b1d6",
-	},
-	brights = {
-		"#414868",
-		"#f7768e",
-		"#9ece6a",
-		"#e0af68",
-		"#7aa2f7",
-		"#bb9af7",
-		"#7dcfff",
-		"#c0caf5",
-	},
+	foreground = "#c8d3f5",
+	background = "#222436",
+	cursor_bg = "#c8d3f5",
+	cursor_border = "#c8d3f5",
+	cursor_fg = "#222436",
+	selection_bg = "#2d3f76",
+	selection_fg = "#c8d3f5",
+	ansi = { "#1b1d2b", "#ff757f", "#c3e88d", "#ffc777", "#82aaff", "#c099ff", "#86e1fc", "#828bb8" },
+	brights = { "#444a73", "#ff757f", "#c3e88d", "#ffc777", "#82aaff", "#c099ff", "#86e1fc", "#c8d3f5" },
 	tab_bar = {
-		inactive_tab_edge = "#16161e",
+		inactive_tab_edge = "#1e2030",
 		background = "#191b28",
 		active_tab = {
-			fg_color = "#7aa2f7",
-			bg_color = "#1a1b26",
+			fg_color = "#82aaff",
+			bg_color = "#222436",
 		},
 		inactive_tab = {
-			bg_color = "#16161e",
+			bg_color = "#1e2030",
 			fg_color = "#545c7e",
 		},
 		inactive_tab_hover = {
-			bg_color = "#16161e",
-			fg_color = "#7aa2f7",
+			bg_color = "#1e2030",
+			fg_color = "#82aaff",
 		},
 		new_tab_hover = {
-			fg_color = "#16161e",
-			bg_color = "#7aa2f7",
+			fg_color = "#1e2030",
+			bg_color = "#82aaff",
 		},
 		new_tab = {
-			fg_color = "#7aa2f7",
+			fg_color = "#82aaff",
 			bg_color = "#191b28",
 		},
 	},
