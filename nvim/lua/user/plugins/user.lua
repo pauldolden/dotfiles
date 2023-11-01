fd370de6028cf35bc23b767c8647ae3c62b6c82b
Paul Dolden <paul.dolden@thefa.com>
2023-11-01 14:12:44 +0000

    add to dot files

---
+++ nvim/lua/user/plugins/user.lua
@@ -0,0 +1,37 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  },
+  { "mg979/vim-visual-multi" },
+  { "APZelos/blamer.nvim" },
+  { "wakatime/vim-wakatime" },
+  {
+    "zbirenbaum/copilot.lua",
+    event = { "InsertEnter" },
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enable = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-f>",
+          },
+        },
+      })
+    end,
+  },
+  { "zbirenbaum/copilot-cmp", after = { "copilot.lua", "nvim-cmp" } },
+}
