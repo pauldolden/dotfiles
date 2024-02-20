11cc79b888313d88828866ad48b6b1698dc5acfd
Paul Dolden <paul@dolden.co.uk>
2024-02-20 05:05:03 +0000

    go back to lazy

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -1,14 +1,4 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
   {
     "folke/tokyonight.nvim",
     lazy = false,
@@ -24,7 +14,7 @@ return {
     "zbirenbaum/copilot.lua",
     event = { "InsertEnter" },
     config = function()
-      require("copilot").setup {
+      require("copilot").setup({
         suggestion = {
           enable = true,
           auto_trigger = true,
@@ -32,7 +22,7 @@ return {
             accept = "<C-f>",
           },
         },
-      }
+      })
     end,
   },
   { "zbirenbaum/copilot-cmp", after = { "copilot.lua", "nvim-cmp" } },
@@ -43,7 +33,9 @@ return {
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = function() require("go").setup() end,
+    config = function()
+      require("go").setup()
+    end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
