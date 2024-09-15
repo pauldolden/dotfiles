066c3da40c9736f27f52c3255526fbe2bcb18e1c
Paul Dolden <paul.dolden@thefa.com>
2024-09-15 09:08:26 +0100

    update

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -5,51 +5,7 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "mg979/vim-visual-multi", lazy = false },
-  { "APZelos/blamer.nvim", lazy = false },
   { "wakatime/vim-wakatime", lazy = false },
-  {
-    "zbirenbaum/copilot.lua",
-    event = { "InsertEnter" },
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enable = true,
-          auto_trigger = true,
-          keymap = {
-            accept = "<C-f>",
-          },
-        },
-      })
-    end,
-  },
-  { "zbirenbaum/copilot-cmp", after = { "copilot.lua", "nvim-cmp" } },
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
 }
