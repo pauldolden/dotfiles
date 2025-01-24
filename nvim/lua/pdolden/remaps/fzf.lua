9747c44dbbcca2f3b725b48939a635520eaedd01
Paul Dolden <paul.dolden@thefa.com>
2025-01-24 09:47:54 +0000

    feat: add hidden files to Fzf

--- nvim/lua/pdolden/remaps/fzf.lua
+++ nvim/lua/pdolden/remaps/fzf.lua
@@ -1,3 +1,4 @@
-vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", { noremap = true, silent = true, desc = "Search files" })
-vim.keymap.set("n", "<leader>/", ":FzfLua grep_project<CR>", { noremap = true, silent = true, desc = "Search in files" })
-vim.keymap.set("n", "<leader>fg", ":FzfLua git_files<CR>", { noremap = true, silent = true, desc = "Search git files" })
+vim.keymap.set("n", "<leader>fp", ":FindProjects<CR>", { noremap = true, silent = true, desc = "Search projects" })
+vim.keymap.set("n", "<leader>ff", ":FzfLua files --hidden<CR>", { noremap = true, silent = true, desc = "Search files including hidden" })
+vim.keymap.set("n", "<leader>/", ":FzfLua live_grep --hidden<CR>", { noremap = true, silent = true, desc = "Search in files including hidden" })
+vim.keymap.set("n", "<leader>fg", ":FzfLua git_files --hidden<CR>", { noremap = true, silent = true, desc = "Search git files including hidden" })
