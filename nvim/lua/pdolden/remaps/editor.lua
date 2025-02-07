b640710e742c5615321461da76f2f6cd561ae48d
Paul Dolden <paul.dolden@thefa.com>
2025-02-07 09:16:42 +0000

    feat: add remap

--- nvim/lua/pdolden/remaps/editor.lua
+++ nvim/lua/pdolden/remaps/editor.lua
@@ -16,3 +16,18 @@ vim.keymap.set("n", "<leader>fb", vim.lsp.buf.format, { noremap = true, silent =
 
 -- Search and replace word under cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { noremap = true, silent = true, desc = "Search and replace word under cursor" })
+
+vim.keymap.set("n", "<leader>R", function()
+  local search_term = vim.fn.getreg("/") -- Get last searched pattern
+  if search_term == "" then
+    print("No search pattern found!")
+    return
+  end
+
+  local replacement = vim.fn.input("Replace '" .. search_term .. "' with: ")
+  if replacement == "" then return end -- Exit if empty replacement
+
+  vim.cmd("vimgrep /\\V" .. search_term .. "/g %") -- Populate quickfix list
+  vim.cmd("cdo s//" .. replacement .. "/gc") -- Confirm replacements
+  vim.cmd("cclose") -- Close quickfix window after replacement
+end, { noremap = true, silent = false, desc = "Search & replace interactively" })
