7f16fb5109cc6a47ca50e5b16e1c4ac390bcc389
Paul Dolden <paul@dolden.co.uk>
2025-12-18 12:34:18 +0000

    feat: changes

--- nvim/lua/pdolden/plugins/core.lua
+++ nvim/lua/pdolden/plugins/core.lua
@@ -120,12 +120,32 @@ return {
     },
   },
   {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = true,
+  },
+  {
     "epwalsh/obsidian.nvim",
     version = "*",
     lazy = true,
     ft = "markdown",
     dependencies = {
       "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New note" },
+      { "<leader>oo", "<cmd>ObsidianSearch<cr>", desc = "Search notes" },
+      { "<leader>os", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick switch" },
+      { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Show backlinks" },
+      { "<leader>ot", "<cmd>ObsidianTags<cr>", desc = "Search tags" },
+      { "<leader>od", "<cmd>ObsidianToday<cr>", desc = "Today's note" },
+      { "<leader>oy", "<cmd>ObsidianYesterday<cr>", desc = "Yesterday's note" },
+      { "<leader>ow", "<cmd>ObsidianWorkspace<cr>", desc = "Switch workspace" },
+      { "<leader>ol", "<cmd>ObsidianLink<cr>", desc = "Link selection", mode = "v" },
+      { "<leader>oln", "<cmd>ObsidianLinkNew<cr>", desc = "Link to new note", mode = "v" },
+      { "<leader>of", "<cmd>ObsidianFollowLink<cr>", desc = "Follow link" },
+      { "<leader>oi", "<cmd>ObsidianPasteImg<cr>", desc = "Paste image" },
+      { "<leader>or", "<cmd>ObsidianRename<cr>", desc = "Rename note" },
     },
     opts = {
       workspaces = {
@@ -142,18 +162,31 @@ return {
         nvim_cmp = true,
         min_chars = 2,
       },
+      -- Use fzf-lua picker (compatible with Snacks)
+      picker = {
+        name = "fzf-lua",
+      },
       mappings = {
+        -- Follow links with gf (works in normal mode on [[links]])
         ["gf"] = {
           action = function()
             return require("obsidian").util.gf_passthrough()
           end,
           opts = { noremap = false, expr = true, buffer = true },
         },
+        -- Toggle checkboxes with <leader>ch
         ["<leader>ch"] = {
           action = function()
             return require("obsidian").util.toggle_checkbox()
           end,
           opts = { buffer = true },
+        },
+        -- Smart action with <CR> (enter key)
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
         },
       },
       ui = {
@@ -161,7 +194,13 @@ return {
         checkboxes = {
           [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
           ["x"] = { char = "", hl_group = "ObsidianDone" },
+          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
         },
+        external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = "ObsidianRefText" },
+        highlight_text = { hl_group = "ObsidianHighlightText" },
+        tags = { hl_group = "ObsidianTag" },
       },
       -- Disable frontmatter if you don't use it, or configure as needed
       disable_frontmatter = false,
